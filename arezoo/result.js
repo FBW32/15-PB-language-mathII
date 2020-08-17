4aa328c2ba7c50eaa4c580396404c26946ff9653
arezoofaraji <arezoo.faraji66@gmail.com>
2020-08-17 14:13:30 +0200

    15 done

---
+++ arezoo/result.js
@@ -0,0 +1,6 @@
+let classRoom = ["Tommy", "Max", "Sara", "Rammy"];
+let classLenght = classRoom.length;
+console.log(classLenght);
+let result = Math.floor(Math.random() * classLenght);
+console.log(result);
+console.log(classRoom[result]);
