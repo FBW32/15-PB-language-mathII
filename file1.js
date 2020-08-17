7e2f9c69859193c91cb4aa8bc91d9439f49710c7
Misbah-D <misbah.deeb@gmail.com>
2020-08-17 12:59:06 +0200

    ok

---
+++ file1.js
@@ -0,0 +1,8 @@
+//  Declare variable classRoom and assign this values ['Tommy','Max', 'Sara', 'Rammy'].
+let classRoom = ['Tommy', 'Max', 'Sara', 'Rammy']
+
+
+//  Create a program that prints a random item from classRoom array.
+randomName = classRoom[Math.floor(Math.random() * classRoom.length)]
+
+console.log(randomName);
